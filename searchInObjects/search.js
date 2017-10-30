1a6443cef9ebfe4f02935ba1be38d328ed1b8b47
mohsen-Nour <mohsen.noor@gmail.com>
2017-10-30 14:39:03 +0000

    adding js search code

--- searchInObjects/search.js
+++ searchInObjects/search.js
@@ -1,1 +1,18 @@
-
+var employeeApp = angular.module("EmployeeApp",[]);
+  employeeApp.controller("empCtrl",function($scope){
+    $scope.query = {}
+    $scope.queryBy = '$'
+    $scope.employees = [
+      {
+        "name" : "Mahesh Pachangane",
+        "company" : "Syntel India Pvt. Ltd",
+        "designation" : "Associate"
+      },
+      {
+        "name" : "Brijesh Shah",
+        "company" : "Britanica Software Ltd.",
+        "designation" : "Software Engineer"
+      }
+    ];
+    $scope.orderProp="name";                
+});
